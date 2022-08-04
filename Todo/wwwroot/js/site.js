4b74cab17edff0794c8055cb055379dcb684964f
David Bankole Abel <ballack_davidabel@yahoo.com>
2022-08-04 16:34:03 -0400

    added some styles

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -3,3 +3,63 @@
 
 // Write your JavaScript code.
 
+function deleteTodo(i) {
+    $.ajax({
+        url: 'Home/Delete',
+        type: 'POST',
+        data: {
+            id: i
+        },
+        success: function () {
+            window.location.reload();
+        }
+    });
+}
+
+//function populateForm(i) {
+
+//    $.ajax({
+//        url: 'Home/PopulateForm',
+//        type: 'GET',
+//        data: {
+//            id: i
+//        },
+//        dataType: 'json',
+//        success: function (response) {
+//            $("#Todo_Name").val(response.name);
+//            $("#Todo_Id").val(response.id);
+//            $("#form-button").val("Update Todo");
+//            $("#form-action").attr("action", "/Home/Update");
+//        }
+//    });
+//}
+
+function completeTodo(i) {
+    $.ajax({
+        url: 'Home/completeTodo',
+        type: 'POST',
+        data: {
+            id: i
+        },
+        success: function () {
+            window.location.reload();
+        }
+    });
+}
+
+//function completeTodo(i) {
+
+//    $.ajax({
+//        url: 'Home/completeTodo',
+//        type: 'GET',
+//        data: {
+//            id: i
+//        },
+//        dataType: 'json',
+//        success: function (response) {
+//            $("#Todo_Name").val(response.name);
+//            $("#Todo_Id").val(response.id);
+//            $("#form-action").attr("action", "/Home/Complete");
+//        }
+//    });
+//}
